d31aa2902947c18326c786119e9e41a7c84058b8
sarah-schmid <sarah.schmid@gather.app>
2022-10-27 16:40:43 -0600

    got interface to render

--- test-app/src/components/interface.js
+++ test-app/src/components/interface.js
@@ -1,8 +1,15 @@
 import Login from "./form.js";
 import Navigation from "./nav-bar.js";
+import Form from "./form.js";
 
 function Interface() {
-  return Login(), Navigation();
+  return (
+    <div>
+      <Navigation />
+      <div dangerouslySetInnerHTML={{ __html: "<h3>Log in</h3>" }} />
+      <Form />
+    </div>
+  );
 }
 
 export default Interface;
