3fe5c2f70442a5cf3b8438edd1d2e9c76e0feef6
sarah-schmid <sarah.schmid@gather.app>
2022-10-27 13:16:33 -0600

    first commit

---
+++ test-app/src/components/interface.js
@@ -0,0 +1,8 @@
+import Login from "./form.js";
+import Navigation from "./nav-bar.js";
+
+function Interface() {
+  return Login(), Navigation();
+}
+
+export default Interface;
---
+++ test-app/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Interface from "./components/interface";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Interface());
